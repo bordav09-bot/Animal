5efb6f5276238853fddeb3a92e502e98f615634e
david thiporsan <bordav09@gmail.com>
2025-11-11 14:32:52 +0200

    javascript משימה 1

---
+++ david/JavaScript.js
@@ -0,0 +1,27 @@
+﻿document.write("something");
+alert("something");
+var num = prompt("הכנס מספר שלם", 7);
+var name = prompt("הכנס את שמך");
+document.write("<br>")
+document.write("hello" + " " + name)
+var age = parseInt(prompt("הכנס את גילך",0))
+var result = age + 1;
+document.write("<br>");
+document.write("Next year you will be" + " " + result + " " + "years old");
+document.write("<br<")
+document.write("תרגיל 13");
+document.write("<br>");
+var number = parseInt(prompt("הכנס מספר", 0));
+var number2 = parseInt(prompt("הכנס מספר", 0));
+var sum = number + number2;
+var sub = number - number2;
+var multy = number * number2;
+var devide = number / number2;
+document.write("<br>");
+document.write("sum =" + " " + sum);
+document.write("<br>");
+document.write("The substantion of the numbers=" + "" + sub);
+document.write("<br>");
+document.write("The multy of both numbers=" + " " + multy);
+document.write("<br>");
+document.write("The numbers devided by each other=" + " " + devide);
